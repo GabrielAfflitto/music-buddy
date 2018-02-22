277bb75c7ef503a9819092fdcebc40db1fa78185
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-22 15:48:44 -0700

    added minimal styling lyrics show page

--- app/models/lyrics.rb
+++ app/models/lyrics.rb
@@ -1,7 +1,7 @@
 class Lyrics
   attr_reader :lyrics, :copyright, :lyrics_url
   def initialize(attrs = {})
-    @lyrics = attrs[:lyrics_body]
+    @lyrics = attrs[:lyrics_body].split("\n")
     @copyright = attrs[:lyrics_copyright]
     @lyrics_url = attrs[:backlink_url]
   end
