e5f26098057d7e1cf45231706a987d6e5988e786
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-24 15:46:31 -0700

    Wrote test for Oauth login

--- spec/features/user_can_log_in_with_google_spec.rb
+++ spec/features/user_can_log_in_with_google_spec.rb
@@ -1,32 +1,30 @@
-# require 'rails_helper'
-#
-# RSpec.feature "user can log in with google oauth" do
-#   def stub_omniauth
-#     OmniAuth.config.test_mode = true
-#     OmniAuth.config.mock_auth[:google] =
-#       OmniAuth::AuthHash.new({
-#         provider: "google",
-#         uid: "122313",
-#         info: {
-#           name: "Gabriel Afflitto",
-#         },
-#         credentials: {
-#           token: "4k3j5h34kj2h234kjh34",
-#           expires_at: Time.now
-#         }
-#       })
-#   end
-#
-#   before :each do
-#     stub_omniauth
-#     # binding.pry
-#   end
-#
-#   it "it should allow user to log in" do
-#     visit root_path
-#     expect(page).to have_content("Sign in with Google")
-#     click_on "Sign in with Google"
-#     expect(page).to have_content("Signed in as Gabriel Afflitto")
-#     expect(page).to have_content("Logout")
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature "user can log in with google oauth" do
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] =
+      OmniAuth::AuthHash.new({provider: "google",
+                              uid: "122313",
+                              info: {
+                                name: "Gabriel Afflitto"
+                                },
+                                credentials: {
+                                  token: "4k3j5h34kj2h234kjh34",
+                                  expires_at: Time.now
+                                  }
+                              })
+  end
+
+  before :each do
+    stub_omniauth
+  end
+
+  it "it should allow user to log in" do
+    visit root_path
+    expect(page).to have_content("Sign in with Google")
+    click_on "Sign in with Google"
+    expect(page).to have_content("Signed in as Gabriel Afflitto")
+    expect(page).to have_content("Sign out")
+  end
+end
