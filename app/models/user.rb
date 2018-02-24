b467db9f1b68c325b64fbbb29f938635f0ea5c10
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-24 11:46:22 -0700

    testing for google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.token_expiration = Time.at(auth.credentials.expires_at)
-      user.save!
-    end
+    # where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+    #   user.provider = auth.provider
+    #   user.uid = auth.uid
+    #   user.name = auth.info.name
+    #   user.oauth_token = auth.credentials.token
+    #   user.token_expiration = Time.at(auth.credentials.expires_at)
+    #   user.save!
+    # binding.pry
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {provider: auth[:provider],
+                       uid: auth[:uid],
+                       name: auth[:info][:name],
+                       oauth_token: auth[:credentials][:token],
+                       token_expiration: auth[:credentials][:token]
+                       }
+    user.save!
+    user
   end
 end
