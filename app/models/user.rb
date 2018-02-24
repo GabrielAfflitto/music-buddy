7df56c99dc9810a27a80cbfa3ccc0d93082ba315
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-24 15:33:01 -0700

    precompiled assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,5 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-    # where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-    #   user.provider = auth.provider
-    #   user.uid = auth.uid
-    #   user.name = auth.info.name
-    #   user.oauth_token = auth.credentials.token
-    #   user.token_expiration = Time.at(auth.credentials.expires_at)
-    #   user.save!
-    # binding.pry
     user = User.find_by(uid: auth[:uid]) || User.new
     user.attributes = {provider: auth[:provider],
                        uid: auth[:uid],
