5a85f8fa76a20543c2ceb8c61d1ec663466fa389
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-24 18:24:17 -0700

    added to simple cov ignore

--- spec/support/simple_cov.rb
+++ spec/support/simple_cov.rb
@@ -3,4 +3,5 @@ SimpleCov.start "rails" do
   add_filter "app/channels/application_cable/connection.rb"
   add_filter "app/jobs/application_job.rb"
   add_filter "app/models/application_record.rb"
+  add_filter "app/mailers/application_mailer.rb"
 end
