583b5f548b35e67474dd8ada2882f12279e1ef9e
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-28 18:07:09 -0700

    added more params to favorite create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
 
   def create
     if current_user
-      @favorite = current_user.favorites.new(lyrics_id: params[:track_id])
+      @favorite = current_user.favorites.new(artist: params[:artist], lyrics_id: params[:track_id], track: params[:track_name])
       @favorite.save!
       flash[:notice] = "Lyrics Saved!"
     end
